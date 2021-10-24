55d3752fab6052b80e992d503227453f22843a37
Gleb Khaykin <khaykingleb@gmail.com>
2021-10-25 00:01:33 +0300

    updated sequential augs

--- asr/augmentations/sequential.py
+++ asr/augmentations/sequential.py
@@ -6,7 +6,8 @@ from torch import Tensor
 
 class SequentialAugmentation:
 
-    AUGMENTATION_PROB = 0.3
+    AUGMENTATION_PROB = 0.8
+    SPECIFIC_AUGMENTATION_PROB = 0.3
 
     def __init__(self, augmentation_list: List[Callable]):
         self.augmentation_list = augmentation_list
@@ -14,8 +15,9 @@ class SequentialAugmentation:
     def __call__(self, data: Tensor) -> Tensor:
         x = data
 
-        for augmentation in self.augmentation_list:
-            if random.random() < self.AUGMENTATION_PROB:
-                x = augmentation(x)
+        if random.random() < self.AUGMENTATION_PROB: 
+            for augmentation in self.augmentation_list:
+                if random.random() < self.SPECIFIC_AUGMENTATION_PROB:
+                    x = augmentation(x)
 
         return x
