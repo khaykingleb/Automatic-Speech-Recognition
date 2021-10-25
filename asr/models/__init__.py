4cddb081d466701d723f6a5c491230b3cc8eff8f
Gleb Khaykin <khaykingleb@gmail.com>
2021-10-25 19:17:21 +0300

    added deepspeech

--- asr/models/__init__.py
+++ asr/models/__init__.py
@@ -1,9 +1,11 @@
 from asr.models.base_model import BaseModel
 from asr.models.dummy_model import DummyModel
 from asr.models.quartznet import QuartzNet
+from asr.models.deepspeech import Deepspeech
 
 __all__ = [
     "BaseModel",
     "DummyModel",
-    "QuartzNet"
+    "QuartzNet",
+    "Deepspeech"
 ]
