6a38c16661441957e7ab658f40db03aad8733197
Gleb Khaykin <khaykingleb@gmail.com>
2021-10-26 14:33:28 +0300

    updated

--- asr/metrics/utils.py
+++ asr/metrics/utils.py
@@ -1,19 +1,18 @@
 import editdistance
 
 
-def calc_cer(target_text: str, predicted_text: str) -> float:
+def calc_wer(target_text: str, pred_text: str) -> float:
     target_words = target_text.split()
-    pred_words = predicted_text.split()
+    pred_words = pred_text.split()
 
-    if len(target_words) != 0:
-        return editdistance.eval(target_words, pred_words) / len(target_words)
-        
-    else:
-        return editdistance.eval(target_words, pred_words)
+    if len(target_words) == 0 and len(pred_words) != 0:
+        return 1
+    
+    return editdistance.eval(target_words, pred_words) / len(target_words)
 
-def calc_wer(target_text: str, predicted_text: str) -> float:
-    if len(target_text) != 0:
-        return editdistance.eval(target_text, predicted_text) / len(target_text)
 
-    else:
-        return editdistance.eval(target_text, predicted_text)
+def calc_cer(target_text: str, pred_text: str):
+    if len(target_text) == 0 and len(pred_text) != 0 :
+        return 1
+    
+    return editdistance.eval(target_text, pred_text) / len(target_text)
