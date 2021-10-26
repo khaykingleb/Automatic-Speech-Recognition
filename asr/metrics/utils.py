6323f5f2874e452a993317e34d37e86784331716
Gleb Khaykin <khaykingleb@gmail.com>
2021-10-26 19:32:05 +0300

    updated

--- asr/metrics/utils.py
+++ asr/metrics/utils.py
@@ -5,14 +5,14 @@ def calc_wer(target_text: str, pred_text: str) -> float:
     target_words = target_text.split()
     pred_words = pred_text.split()
 
-    if len(target_words) == 0 and len(pred_words) != 0:
+    if len(target_words) == 0:
         return 1
     
     return editdistance.eval(target_words, pred_words) / len(target_words)
 
 
 def calc_cer(target_text: str, pred_text: str):
-    if len(target_text) == 0 and len(pred_text) != 0 :
+    if len(target_text) == 0 :
         return 1
     
     return editdistance.eval(target_text, pred_text) / len(target_text)
