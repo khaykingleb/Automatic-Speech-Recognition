f3e0730660752c06d03b7f8d2e9c77bffb449f70
Gleb Khaykin <khaykingleb@gmail.com>
2021-10-28 02:19:38 +0300

    updated

--- setup.py
+++ setup.py
@@ -1,16 +1,12 @@
 from setuptools import find_packages, setup
 
 
-setup(
-    name="asr",
-    version="1.0",
-    author="khaykingleb",
-    package_dir={"": "asr"},
-    packages=find_packages("asr"),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-)
+setup(name="asr",
+      version="1.0",
+      author="khaykingleb",
+      package_dir={"": "asr"},
+      packages=find_packages("asr"),
+      classifiers=["Programming Language :: Python :: 3",
+                   "License :: OSI Approved :: MIT License",
+                   "Operating System :: OS Independent"],
+      python_requires='>=3.6')
