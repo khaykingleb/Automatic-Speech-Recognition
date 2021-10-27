139cf609a8bc74d16d081d30c0cf137e514d1bbb
Gleb Khaykin <khaykingleb@gmail.com>
2021-10-27 20:37:30 +0300

    updated

--- asr/datasets/__init__.py
+++ asr/datasets/__init__.py
@@ -1,9 +1,13 @@
 from asr.datasets.librispeech_dataset import LibrispeechDataset
 from asr.datasets.ljspeech_dataset import LJSpeechDataset
 from asr.datasets.mozilla_common_voice_dataset import MozillaCommonVoice
+from asr.datasets.custom_audio_dataset import CustomAudioDataset
+from asr.datasets.custom_dir_audio_dataset import CustomDirAudioDataset
 
 __all__ = [
     "LibrispeechDataset",
-    "LJSpeechDataset"
-    "MozillaCommonVoice"
+    "LJSpeechDataset",
+    "MozillaCommonVoice",
+    "CustomAudioDataset",
+    "CustomDirAudioDataset"
 ]
